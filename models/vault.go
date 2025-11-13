53494a67d3b779215e8b2a1c587efe1c4e232916
ToEzBit <jirayut52328172@hotmail.com>
2025-11-13 14:42:07 +0700

    :card_file_box: model

---
+++ models/vault.go
@@ -0,0 +1,21 @@
+package models
+
+type Vault struct {
+	ID                   string
+	WorkSpaceName        string
+	WorkSpaceDescription string
+	Credentials          []Credential
+}
+
+type Credential struct {
+	ID       string
+	AcountId string
+	Password string
+	App      string
+	Url      string
+	note     string
+}
+
+type ValutData struct {
+	Valuts []Vault
+}
